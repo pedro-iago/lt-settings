070cbfc816d01d70e2d00008f433e2f0c016f0dd
pedro <pedro.m4rtins@gmail.com>
2016-04-28 13:38:59 -0300

    update

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ Add these files to your ~/.config/LightTable/User folder.
 Make sure you have Java and the Leiningen script.
 Visit:
 http://askubuntu.com/questions/122209/how-do-i-modify-or-disable-the-huds-use-of-the-alt-key
+
 https://github.com/pallet/alembic
+
 https://github.com/kuznicki-me/gitlight/issues/3
---
+++ src/lt/plugins/user.cljs
@@ -0,0 +1,35 @@
+(ns lt.plugins.user
+  (:require [lt.object :as object]
+            [lt.objs.tabs :as tabs]
+            [lt.objs.command :as cmd])
+  (:require-macros [lt.macros :refer [defui behavior]]))
+
+;; UI to be associated with an object
+(defui hello-panel [this]
+  [:h1 "Hello World!"])
+
+;; Define an object prototype
+(object/object* ::user.hello
+                :tags [:user.hello]
+                :behaviors [::on-close-destroy]
+                :init (fn [this]
+                        (hello-panel this)))
+
+;; Currently used by :user.hello but could be reused by any
+;; object with a declaration in user.behaviors.
+(behavior ::on-close-destroy
+          :triggers #{:close}
+          :reaction (fn [this]
+                      (object/raise this :destroy)))
+
+(def hello (object/create ::user.hello))
+
+;; Create a user command. Commands can call any function
+;; and be bound to any keystroke.
+(cmd/command {:command :user.say-hello
+              :desc "User: Say Hello"
+              :exec (fn []
+                      (tabs/add-or-focus! hello))})
+
+;; :browser.url-bar.focus or :browser.focus-content on add-browser (go to google.com)
+;; on-add-browser move to tabset 2, on-add-editor move to tabset 1 (focus)
